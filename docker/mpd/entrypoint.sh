f932072bf75e6a406e05b48fb8df59a8f6446a65
William Forster <william@intelligentpositioning.com>
2026-03-28 23:01:37 +0000

    Make MPD audio device configurable via MPD_AUDIO_DEVICE env var

--- docker/mpd/entrypoint.sh
+++ docker/mpd/entrypoint.sh
@@ -3,4 +3,8 @@
 # so the file must be created at runtime, not just in the image build.
 touch /var/lib/mpd/database
 
+# Substitute environment variables into the MPD config at runtime.
+# This allows MPD_AUDIO_DEVICE to differ between dev and production.
+sed -i "s|\$MPD_AUDIO_DEVICE|${MPD_AUDIO_DEVICE}|g" /etc/mpd.conf
+
 exec "$@"
