5a2e44e458195e781c90f7aef1657549e1972611
William Forster <william@intelligentpositioning.com>
2026-03-28 23:13:25 +0000

    Make MPD mixer type configurable, use hardware mixer on NUC

--- docker/mpd/entrypoint.sh
+++ docker/mpd/entrypoint.sh
@@ -6,5 +6,6 @@ touch /var/lib/mpd/database
 # Substitute environment variables into the MPD config at runtime.
 # This allows MPD_AUDIO_DEVICE to differ between dev and production.
 sed -i "s|\$MPD_AUDIO_DEVICE|${MPD_AUDIO_DEVICE}|g" /etc/mpd.conf
+sed -i "s|\$MPD_MIXER_TYPE|${MPD_MIXER_TYPE}|g" /etc/mpd.conf
 
 exec "$@"
